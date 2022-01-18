59fbc4b4be537d59cca9633ae0f7e4a61b26c86a
Kadir Yılmaz <kadirylmaz02@gmail.com>
2022-01-18 13:47:41 +0300

    Create tables for Product database

---
+++ Services/Mango.Services.ProductAPI/Data/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Mango.Services.ProductAPI.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.ProductAPI.Data.DbContexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
